64fee941630ff92a1279fd9da72df6bc22d73757
Fernando Flauzino <fernando.flauzino@atlasproj.com>
2019-05-18 16:25:17 -0300

    trabalho controles basicos

---
+++ XF.ControlesBasicos/XF.ControlesBasicos/Configuracao.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XF.ControlesBasicos
+{
+    public class Configuracao
+    {
+        public string Email { get; set; }
+        public bool SendEmail { get; set; }
+    }
+}
---
+++ XF.ControlesBasicos/XF.ControlesBasicos/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XF.ControlesBasicos
+{
+    public partial class MainPage : ContentPage
+    {
+        private Configuracao _vmConfig;
+        public MainPage()
+        {
+            if (_vmConfig == null)
+                _vmConfig = new Configuracao();
+
+            InitializeComponent();
+        }
+
+        private void BtnEnviar_Clicked(object sender, EventArgs e)
+        {
+            if (_vmConfig.SendEmail)
+            {
+                DisplayAlert("Atenção", $"E-mail enviado para {_vmConfig.Email}", "OK");
+            }
+            else
+            {
+                DisplayAlert("Atenção", "E-mail não autorizado!", "OK");
+            }
+               
+        }
+
+        private void BtnConfigurar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ConfigPage() { BindingContext = _vmConfig });
+        }
+    }
+}
